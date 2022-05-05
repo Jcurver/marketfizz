7bed54d1588529fde4c97d48f72b88b11cacd234
박재현 <eric7213@naver.com>
2022-05-05 21:08:35 +0900

    form

---
+++ libs/client.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+
+// const client = new PrismaClient();
+
+// client.user.create({ data: { email: "eric7213@naver.com", name: "fizzhyun" } });
+
+export default new PrismaClient();
